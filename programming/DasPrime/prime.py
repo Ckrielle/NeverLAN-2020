039495fac1691418d9b8f3642857fbf8d9ab0a28
Ckrielle <sraabooshki@gmail.com>
2020-02-12 22:24:44 +0200

    Updated prime.py

--- programming/DasPrime/prime.py
+++ programming/DasPrime/prime.py
@@ -1,20 +1,28 @@
 import math
+
 def main():
 
     primes = []
     count = 2
-    index = 0
+    index = 1
 
     while True:
+        
         isprime = True
+        
         for x in range(2, count):#int(math.sqrt(count) + 1)):
             if count % x == 0: 
                 isprime = False
                 continue
+                
         if isprime:
             primes.append(count)
-            print(index, primes[index])
+            #print(index, primes[index])
             index += 1
+            
+        if index = 10497:
+            print(primes[index])
+           
         count += 1
 
 if __name__ == "__main__":
