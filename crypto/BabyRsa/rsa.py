8c6c36edfb1178a807ce5e2a9bb6a291c570e47e
Ckrielle <sraabooshki@gmail.com>
2020-02-12 21:50:35 +0200

    Fixed rsa.py

--- crypto/BabyRsa/rsa.py
+++ crypto/BabyRsa/rsa.py
@@ -1,19 +1,20 @@
 from pwn import *
 import gmpy
-
-if __name__ == '__main__':
    
-    c = 21931745216497014662495143814121745174523021163218116131438884249523022164218188423021703192423021801141224955313372217
-    e = 569
-    n = 2533
+c = "2193 1745 2164 970 1466 2495 1438 1412 1745 1745 2302 1163 2181 1613 1438 884 2495 2302 2164 2181 884 2302 1703 1924 2302 1801 1412 2495 53 1337 2217".split()
+e = 569
+n = 2533
    
-    #Since n is really small, we can factor it in e.g. factorb
-    p = 17
-    q = 149
+#Since n is really small, we can factor it in e.g. factorb
+p = 17
+q = 149
 
-    phi = (p-1) * (q-1)
-    d = gmpy.invert(e, phi)
+phi = (p-1) * (q-1)
+d = gmpy.invert(e, phi)
 
-    end = hex(pow(c, d, n))
-    print(str(end))
+m = ''
+for i in c:
+   m += chr(pow(int(i), d, n))
+
+print(m)
 
